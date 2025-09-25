855a983131d4b464987f5a3d00477c291239dc15
Luka <khundadzeluka702@gmail.com>
2025-09-25 14:33:58 +0400

    UI updates

--- Frontend/PlaylistConverter/src/components/Navbar/Navbar.tsx
+++ Frontend/PlaylistConverter/src/components/Navbar/Navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar()
                             Login
                         </NavLink>
                     ) : (
-                        <button className={styles.link} onClick={handleLogout}>
+                        <button className={`${styles.link} ${styles.logoutBtn}`} onClick={handleLogout}>
                             Logout
                         </button>
                     )}
